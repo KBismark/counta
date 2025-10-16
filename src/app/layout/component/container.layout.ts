31c13fac449686eb95f155aacee44124e92d9d66
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2025-10-16 17:43:44 +0000

    make products page responsive

--- src/app/layout/component/container.layout.ts
+++ src/app/layout/component/container.layout.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'screen-container',
     imports: [CommonModule],
-    template: `<div class="container xs:max-w-full mx-auto xl:w-full! xl:max-w-[1440px]! max-md:bg-white py-6 mt-4  px-2.5 xl:px-6 space-y-12">
+    template: `<div class="container xs:max-w-full mx-auto xl:w-full! xl:max-w-[1440px]! max-md:bg-white py-6 mt-4 max-md:-mt-12 px-2.5 xl:px-6 space-y-12">
     <ng-content></ng-content>
   </div>`,
 })
